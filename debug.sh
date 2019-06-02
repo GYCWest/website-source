0ad0cc6afd3439b8cd0f8c7ce4e3b739f9c2599a
Ricky C <kf6kjg@users.noreply.github.com>
2019-06-02 15:24:30 -0700

    Cross platform debug launch script

--- debug.sh
+++ debug.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-xdg-open http://localhost:5341/ &
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+	xdg-open http://localhost:5341/ &
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+	open http://localhost:5341/ &
+else # Windows?
+	start http://localhost:5341/ &
+fi
 
 hugo server --baseURL 0.0.0.0 --bind 0.0.0.0 --port 5341 --watch --cleanDestinationDir $@
